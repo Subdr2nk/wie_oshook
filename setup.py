8b3bddd314c31d5bbe35d80bac5495c6065a0687
Subdr2nk <136206803+Subdr2nk@users.noreply.github.com>
2023-08-20 16:26:59 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+        name="wie_oshook",
+        version="1.2.6.0",
+        description="A private hook",
+        packages=[],
+        author="Subdr2nk",
+        author_email="subdr2nk@gmail.com",
+        license="LICENSE.txt",
+        url="https://github.com/Subdr2nk/wie_oshook/"
+        zip_safe=False)
